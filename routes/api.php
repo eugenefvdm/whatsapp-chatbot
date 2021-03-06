8e809a68479711695233550f0c0ab3cb0ceb9b07
Eugene <eugenevdm@gmail.com>
2021-03-06 11:36:00 +0200

    fix namespacing

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/chat-bot', 'ChatBotController@listenToReplies');
+Route::post('/chat-bot', 'App\Http\Controllers\ChatBotController@listenToReplies');
